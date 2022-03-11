550027651728883e902248f76e5fdf572e46785d
Hiren Khurana <khuranahiren@gmail.com>
2022-03-11 06:23:16 -0800

    Final

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ st.image('assets/BB-logo.jpg')
 #run = st.checkbox('Scan')
 run = st.slider('Scan', 0, 1, 1)
 
+<<<<<<< Updated upstream
+=======
+st.markdown('<style>body{background-color: #2a4cba;}</style>',unsafe_allow_html=True)
+>>>>>>> Stashed changes
 st.markdown(
     f'''
         <style>
@@ -47,41 +51,35 @@ while run:
                 df = pandas.read_csv('assets/laptops.csv')
 
                 id_row =  df.loc[df['ID'] == allids[0]]
-                st.title(id_row.loc[0 , 'brand'] + ' ' + id_row.loc[0, 'model'])
-                st.title(id_row.loc[0 , 'Price'])
-                st.image(id_row.loc[0 , 'Image'])
+                st.title(id_row.loc[id_row.index.values[0] , 'brand'] + ' ' + id_row.loc[id_row.index.values[0], 'model'])
+                st.header(id_row.loc[id_row.index.values[0] , 'Price'])
+                st.image(id_row.loc[id_row.index.values[0] , 'Image'])
 
-                st.markdown("Release Year : " + str(id_row.loc[0, 'Release Year']))
-                st.markdown("Display : " + id_row.loc[0, 'Display'])
-                st.markdown("Weight : " + id_row.loc[0, 'Weight'])
-                st.markdown("Graphics : " + id_row.loc[0, 'Graphic Card'])
-                st.markdown("Processor : " + id_row.loc[0, 'Processor'])
+                st.subheader('Release Year : ' + str(id_row.loc[id_row.index.values[0], 'Release Year']))
+                st.subheader("Display : " + id_row.loc[id_row.index.values[0], 'Display'])
+                st.subheader("Weight : " + id_row.loc[id_row.index.values[0], 'Weight'])
+                st.subheader("Graphics : " + id_row.loc[id_row.index.values[0], 'Graphic Card'])
+                st.subheader("Processor : " + id_row.loc[id_row.index.values[0], 'Processor'])
+                st.title("------------------------------------------------")
                 
 
             else:
                 df = pandas.read_csv('assets/phones.csv') 
 
                 id_row =  df.loc[df['ID'] == allids[0]]
-                st.title(id_row.loc[0 , 'brand'] + ' ' + id_row.loc[0, 'model'])
-                st.title(id_row.loc[0 , 'Price'])
-                st.image(id_row.loc[0 , 'Image'])
+                st.title(id_row.loc[id_row.index.values[0] , 'brand'] + ' ' + id_row.loc[id_row.index.values[0], 'model'])
+                st.header(id_row.loc[id_row.index.values[0] , 'Price'])
+                st.image(id_row.loc[id_row.index.values[0] , 'Image'])
 
-                st.markdown("Release date : " + id_row.loc[0, 'release date'])
-                st.markdown("Refresh rate : " + id_row.loc[0, 'Refresh Rate'] )
-                st.markdown("Camera : " + id_row.loc[0, 'Camera'])
-                st.markdown("Battery : " + id_row.loc[0, 'Battery'])
-                st.markdown("Storage options : " + id_row.loc[0, 'Storage options'])
-                st.markdown("Colours : " + id_row.loc[0, 'Colours'])
+                st.subheader("Release date : " + id_row.loc[id_row.index.values[0], 'release date'])
+                st.subheader("Refresh rate : " + id_row.loc[id_row.index.values[0], 'Refresh Rate'] )
+                st.subheader("Camera : " + id_row.loc[id_row.index.values[0], 'Camera'])
+                st.subheader("Battery : " + id_row.loc[id_row.index.values[0], 'Battery'])
+                st.subheader("Storage options : " + id_row.loc[id_row.index.values[0], 'Storage options'])
+                st.subheader("Colours : " + id_row.loc[id_row.index.values[0], 'Colours'])
+                st.title("------------------------------------------------")
+
+
 else:
     st.markdown(cur_object)
-
-
     
-
-
-
-
-
-
-
-
