46c82b4a29755fc4c84ba7b1ceeadf322631c031
Deepan  Chakravarthy <70298670+deepan19@users.noreply.github.com>
2022-03-10 19:32:47 -0800

    qrcode detection

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import fileinput
 import streamlit as st
 import numpy as np
+import sys, qrcode
 from PIL import Image
+import cv2
 
 st.title('Better Buy')
 
@@ -12,6 +14,35 @@ if fileup:
     image = Image.open(fileup)
     
     st.image(image, use_column_width=True)
+
+    qrCodeDetector = cv2.QRCodeDetector()
+
+    decodedText, points, _ = qrCodeDetector.detectAndDecode(image)
+
+    if points is not None:
+ 
+        nrOfPoints = len(points)
+    
+        for i in range(nrOfPoints):
+            nextPointIndex = (i+1) % nrOfPoints
+            cv2.line(image, tuple(points[i][0]), tuple(points[nextPointIndex][0]), (255,0,0), 5)
+    
+        print(decodedText)    
+    
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+     
+    else:
+        print("QR code not detected")
+
+
+
+    
+
+
+
+
 
 
 
