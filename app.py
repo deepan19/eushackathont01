81b2047d86319fd8792bdf5cf6adef496d386c99
Hiren Khurana <khuranahiren@gmail.com>
2022-03-11 02:10:41 -0800

    video-test

--- app.py
+++ app.py
@@ -4,87 +4,43 @@ import numpy as np
 from PIL import Image
 import cv2
 import pyzbar.pyzbar as pyzbar
-import pandas
+from streamlit_webrtc import webrtc_streamer
 
 
 frame_skip = 300
 st.image('assets/BB-logo.jpg')
 
-#run = st.checkbox('Scan')
-run = st.slider('Scan', 0, 1, 1)
+webrtc_streamer(key="example")
 
-st.markdown(
-    f'''
-        <style>
-            .sidebar .sidebar-content {{
-                width: 375px;
-            }}
-        </style>
-    ''',
-    unsafe_allow_html=True
-)
+# #run = st.checkbox('Scan')
+# run = st.slider('Scan', 0, 1, 1)
 
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
+# st.markdown(
+#     f'''
+#         <style>
+#             .sidebar .sidebar-content {{
+#                 width: 375px;
+#             }}
+#         </style>
+#     ''',
+#     unsafe_allow_html=True
+# )
 
-cur_frame = 0
-cur_object = 'Stopped'
+# FRAME_WINDOW = st.image([])
+# camera = cv2.VideoCapture(0)
 
-while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
-    decodedObjects = pyzbar.decode(frame)
-    if len(decodedObjects) > 0:
-        if(cur_object != decodedObjects[0].data.decode()):
-            cur_object = decodedObjects[0].data.decode()
-            
-            device = cur_object.split('-',1)[0] == 'l'
+# cur_frame = 0
+# cur_object = 'Stopped'
 
-            allids = cur_object.split('\n')
-
-            if(device):
-                df = pandas.read_csv('assets/laptops.csv')
-
-                id_row =  df.loc[df['ID'] == allids[0]]
-                st.title(id_row['Brand'] + ' ' + id_row['Model'])
-                st.title(id_row['Price'])
-                st.image(id_row['Image'])
-
-                st.markdown("Release Year : " + id_row['Release Year'])
-                st.markdown("Display : " + id_row['Display'])
-                st.markdown("Weight : " + id_row['Weight'])
-                st.markdown("Graphics : " + id_row['Graphic Card'])
-                st.markdown("Processor : " + id_row['Processor'])
-                
-
-            else:
-                df = pandas.read_csv('assets/phones.csv') 
-
-                st.title(id_row['Brand'] + ' ' + id_row['Model'])
-                st.title(id_row['Price'])
-                st.image(id_row['Image'])
-
-                st.markdown("Release date : " + id_row['Release date'])
-                st.markdown("Camera : " + id_row['Camera'])
-                st.markdown("Battery : " + id_row['Battery'])
-                st.markdown("Storage options : " + id_row['Storage options'])
-                st.markdown("Colours : " + id_row['Colours'])
-
-
-            st.markdown()
-
-            st.markdown(decodedObjects[0].data.decode())
-else:
-    st.markdown(cur_object)
-
-
-    
-
-
-
-
-
-
-
-
+# while run:
+#     _, frame = camera.read()
+#     if(frame is not None):
+#         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#         FRAME_WINDOW.image(frame)
+#         decodedObjects = pyzbar.decode(frame)
+#         if len(decodedObjects) > 0:
+#             if(cur_object != decodedObjects[0].data.decode()):
+#                 cur_object = decodedObjects[0].data.decode()
+#                 st.markdown(decodedObjects[0].data.decode())
+# else:
+#     st.markdown(cur_object)
